d3dbf55a6b6c78d28906504a3843a440d507fd75
peluko00 <91435961+peluko00@users.noreply.github.com>
2022-11-18 18:17:21 +0100

    feat: Add new preview page

--- public_html/js/processor.js
+++ public_html/js/processor.js
@@ -3,24 +3,32 @@ var video
 
 function obtain_video_local() {
     const path_local = document.getElementById("video").value;
-    
+
     if (path_local == ''){
         alert("No s'ha introduit un video")
     }
     else {
-        console.log('hola')
+        video = path_local
+        window.location.href = '../preview.html'
+        put_video_preview()
     }
 }
 
 function obtain_video_url() {
-    //const url_web = document.getElementById("url").value;
-    const url_web = document.getElementById("video").value;
-    
+    const url_web = document.getElementById("url_video").value;
     if (url_web == ''){
         alert("No s'ha introduit un video")
     }
     else {
-
-        console.log('hola')
+        video = url_web
+        window.location.href = '../preview.html'
+        put_video_preview()
     }
+}
+
+function put_video_preview() {
+    const video_preview = document.getElementById("video_preview");
+    console.log(video)
+    video_preview.src = video
+
 }
