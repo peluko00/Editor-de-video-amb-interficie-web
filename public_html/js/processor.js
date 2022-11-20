5b3a8fb021341a313e3a6e81b903c27f0e3e213f
peluko00 <91435961+peluko00@users.noreply.github.com>
2022-11-20 11:48:58 +0100

    feat: Add checkboxes

--- public_html/js/processor.js
+++ public_html/js/processor.js
@@ -1,28 +1,10 @@
-
-function obtain_video_local() {
-    show(document.getElementById('vd'));
-
-
-
-    const path_local = document.getElementById("video").value;
-    const video = document.getElementById("video").files[0];
-
-    if (path_local == ''){
-        alert("No s'ha introduit un video")
-    }
-    else {
-        console.log(path_local)
-        const videoURL = URL.createObjectURL(video);
-        document.querySelector('.profile img').setAttribute('src', videoURL);
-
-
-        video.src = path_local
-        localStorage.setItem("video", path_local)
-    }
-}
-
 const getValuesFromInputs = () =>{
-    const profileAud = document.querySelector('input.profile-aud').files[0];
+    const profileAud = document.querySelector('input.video').files[0];
+
+    if (profileAud == null){
+        alert("No s'ha introduit un video")
+        return ;
+    }
 
     document.querySelector('form').style.display = 'none';
 
@@ -32,6 +14,7 @@ const getValuesFromInputs = () =>{
 
 const convertInputValues = () => {
     const profileAud = getValuesFromInputs();
+
 
     const profileAudURL = URL.createObjectURL(profileAud);
 
@@ -43,12 +26,40 @@ const addInputToProfile = () => {
 
     const profileAudURL  = convertInputValues();
 
-    document.querySelector('.aud').setAttribute('src', profileAudURL);
+    document.querySelector('.video').setAttribute('src', profileAudURL);
 
     document.querySelector('.header').style.display = 'flex';
 }
 
-document.querySelector('button').addEventListener('click', (e) => {
+document.getElementById('upload').addEventListener('click', (e) => {
     e.preventDefault();
     addInputToProfile();
 });
+
+document.getElementById('edita').addEventListener('click', (e) => {
+    e.preventDefault();
+    validateOptions()
+});
+
+function validateOptions () {
+    const escalar = document.getElementById("escalar");
+    const bitrate = document.getElementById("bitrate");
+    const velocitat = document.getElementById("velocitat");
+
+    console.log(escalar)
+
+    var checkboxes = []
+
+    if (escalar.checked){
+        checkboxes.push('escalar')
+    }
+    if (bitrate.checked){
+        checkboxes.push('bitrate')
+    }
+    if (velocitat.checked){
+        checkboxes.push('velocitat')
+    }
+    if(checkboxes.length === 0) {
+        alert("S'ha de seleccionar com a minim una opció");
+    }
+}
